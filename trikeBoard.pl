6f7db22864ecf43ca347513fabb51f871973742f
koaLaMoucho <koalamoucho@gmail.com>
2023-10-30 12:04:29 +0000

    validating moves

--- trikeBoard.pl
+++ trikeBoard.pl
@@ -18,9 +18,30 @@ row_of_vars(N, [Var | Rest]) :-
     row_of_vars(NextN, Rest),
     Var = 'X'. % You can use any variable here
 
-% Update a value at a specific position in the matrix
+% Update a value at a specific position in the matrix with move restrictions
 update_matrix(Matrix, Row, Col, NewValue, UpdatedMatrix) :-
-    update_row(Matrix, Row, Col, NewValue, UpdatedMatrix).
+    % Keep track of the last row and last column
+    last_move(LastRow, LastCol),
+    
+    % Add move restrictions here
+    (valid_move(Row, Col, LastRow, LastCol) ; throw(error('Invalid move: Rows or columns must be the same.'))),
+    update_row(Matrix, Row, Col, NewValue, UpdatedMatrix),
+
+    % Update the last row and last column
+    retractall(last_move(_, _)),
+    asserta(last_move(Row, Col)).
+
+% Define the initial last move
+:- dynamic last_move/2.
+last_move(7, 1).
+
+valid_move(Row, Col, LastRow, LastCol) :-
+    Row > 0,
+    Col > 0,
+    Row =< 7, % Adjust this based on your matrix size
+    Col =< Row, % Adjust this based on your matrix size
+    (Row =\= LastRow ; Col =\= LastCol),
+    (Row =:= LastRow ; Col =:= LastCol).
 
 update_row([CurrentRow | Rest], 1, Col, NewValue, [UpdatedRow | Rest]) :-
     update_column(CurrentRow, Col, NewValue, UpdatedRow).
@@ -51,7 +72,6 @@ display_matrix(Matrix) :-
     display_column_numbers(1, BoardSize),
     nl.
     
-
 display_matrix([], _).
 display_matrix([Row | Rest], RowNum) :-
     display_row_number(RowNum),
@@ -82,22 +102,50 @@ display_row([Cell | Rest]) :-
     write(' '),
     display_row(Rest).
 
-
 % Example of the initial game state for Trike with a 7x7 board.
 initial_state(Size, [Board, CurrentPlayer]) :-
     create_matrix(Size, Board),
     CurrentPlayer = white.
 
+% Display the last move
+display_last_move :-
+    last_move(LastRow, LastCol),
+    format('Last Move: Row ~d, Column ~d~n', [LastRow, LastCol]).
+
+% Generate a list of integers between L and H
+generate_range(L, H, [L | Rest]) :-
+    L < H,
+    NextL is L + 1,
+    generate_range(NextL, H, Rest).
+generate_range(H, H, [H]).
+
+% Generate a list of available moves
+available_moves(Matrix, AvailableMoves) :-
+    last_move(LastRow, LastCol),
+    length(Matrix, Size),
+    generate_range(1, Size, Rows),
+    generate_range(1, Size, Cols),
+    findall([Row, Col], (member(Row, Rows), member(Col, Cols), valid_move(Row, Col, LastRow, LastCol)), AvailableMoves).
+
+
 % Example of how to use the update_matrix predicate
 example_update :-
     create_matrix(7, Matrix),
     display_game([Matrix, white]), % Display the initial game state
+    display_last_move, % Display the last move
     nl,
-    write('Enter the row to update'),
+    
+    % Generate and display a list of available moves
+    available_moves(Matrix, AvailableMoves),
+    format('Available Moves: ~w~n', [AvailableMoves]),
+    
+    write('Enter the row to update: '),
     read(Row),
-    write('Enter the column to update'),
+    write('Enter the column to update: '),
     read(Column),
-    write('Enter the new value'),
-    read(NewValue),
-    update_matrix(Matrix, Row, Column, NewValue, UpdatedMatrix), % Update a value
+    catch(
+        update_matrix(Matrix, Row, Column, 'O', UpdatedMatrix), % Update a value (using 'O' for demonstration)
+        error(Reason, _),
+        (nl, write('Error: '), write(Reason), nl)
+    ),
     display_game([UpdatedMatrix, black]). % Display the updated game state with black as the current player
