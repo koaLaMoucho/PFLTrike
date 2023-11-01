bcb965d7250d96b3c278a3a998b34eea451e1261
koaLaMoucho <koalamoucho@gmail.com>
2023-11-01 12:42:45 +0000

    merging board and testing win failed

--- trikeBoard.pl
+++ trikeBoard.pl
@@ -2,7 +2,6 @@
 :- use_module(library(random)).
 :- use_module(library(between)).
 :- use_module(library(system)).
-:- use_module(library(not)).
 
 is_empty_list([]). % to easily see if a list is empty
 
@@ -66,19 +65,28 @@ display_game([Board, CurrentPlayer]) :-
 
 display_matrix(Matrix) :-
     length(Matrix, BoardSize),
-    display_matrix(Matrix, 1),
+    display_matrix(Matrix, 1, BoardSize),
     nl,
-    write('  '),
+    write(' '),
     display_column_numbers(1, BoardSize),
     nl.
 
-display_matrix([], _).
-display_matrix([Row | Rest], RowNum) :-
+display_matrix([], _, _).
+display_matrix([Row | Rest], RowNum, BoardSize) :-
+    SpacesBefore is (BoardSize - RowNum) * 2,
     display_row_number(RowNum),
+    display_spaces(SpacesBefore),
     display_row(Row),
     nl,
     NextRowNum is RowNum + 1,
-    display_matrix(Rest, NextRowNum).
+    display_matrix(Rest, NextRowNum,BoardSize).
+
+display_spaces(0).
+display_spaces(N) :-
+    N > 0,
+    write(' '),
+    NextN is N - 1,
+    display_spaces(NextN).
 
 display_column_numbers(CurrentCol, MaxCol) :-
     CurrentCol > MaxCol,
@@ -97,9 +105,9 @@ display_row_number(RowNum) :-
 
 display_row([]).
 display_row([Cell | Rest]) :-
-    write('| '),
-    write(Cell),
     write(' '),
+    write(Cell),
+    write('  '),
     display_row(Rest).
 /*
 % Example of the initial game state for Trike with a 7x7 board.
